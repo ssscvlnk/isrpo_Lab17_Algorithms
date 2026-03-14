7f530d5ea4a6ecf18cb9b67aca5ead845576ea91
ssscvlnk <pstartceva@yandex.ru>
2026-03-14 11:50:13 +0300

    Step 7 arrays

---
+++ code/Step7_ArrayElements/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Вывод всех элементов массива");
+int[] array = { 5, 4, 5, 3, 4 };
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
---
+++ code/Step7_ArrayMax/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Вывод максимального элемента массива");
+int[] array = { 5, 4, 5, 3, 4 };
+int max = array[0];
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+Console.WriteLine($"Максимальный элемент: {max}");
